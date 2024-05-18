d497389786802ca87edf5c322f94c364bcaa777f
jasurbek22 <155670277+Jasurbek-git@users.noreply.github.com>
2024-05-18 23:15:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const animatedElements = document.querySelectorAll('.animated');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Отключаем наблюдение после появления
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    animatedElements.forEach(element => {
+        observer.observe(element);
+    });
+});
